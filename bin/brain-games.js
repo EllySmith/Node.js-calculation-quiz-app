8063d86ab3acd72326cb94bfe45f41bbdd26468a
Alexandra Adelson <ellysmith@Alexandras-MacBook-Pro.local>
2022-08-12 17:53:08 +0200

    even number game add

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,4 +3,4 @@ import readlineSync from 'readline-sync';
 
 import * as dialog from '../src/cli.js';
 
-
+import * as evennumber from './brain-even.js';
--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,6 @@ import readlineSync from 'readline-sync';
 
 console.log("Welcome to the Brain Games!\nMay I have your name?");
 
-const name = readlineSync.question('Your answer: ');
+export const name = readlineSync.question('Your answer: ');
 
-console.log("Hello, "+name+"!")
-
+console.log("Hello, "+name+"!");
