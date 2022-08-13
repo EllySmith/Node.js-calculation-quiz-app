f716af3135656227810e090f8367f4e1c1fb8158
Alexandra Adelson <ellysmith@Alexandras-MacBook-Pro.local>
2022-08-13 02:02:22 +0200

    prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,47 @@
+import readlineSync from 'readline-sync';
+
+import * as dialog from '../src/cli.js';
+
+import { name } from '../src/cli.js';
+
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+const randomNumber = Math.floor(Math.random() * 15);
+
+console.log(`Question ${randomNumber}`);
+
+
+let primeOrNot = 'no';
+
+for (let i = 2; i < randomNumber/2 ; i++) {
+    if (randomNumber === 2) {
+        primeOrNot = 'yes';
+        break;
+    }
+    if (randomNumber === 3) {
+        primeOrNot = 'yes';
+        break;
+    }
+    if (randomNumber % i === 0) {
+        primeOrNot = 'no';
+        break;
+    }
+
+    if (randomNumber % i !== 0) {
+        primeOrNot = 'yes';
+        
+    }
+    primeOrNot = 'yes';
+    break;
+    
+}
+
+const userGuess = readlineSync.question('Your answer: ');
+
+if (userGuess === primeOrNot) {
+    console.log('Correct!');
+}
+
+if (userGuess !== primeOrNot) {
+    console.log(`'${userGuess}' is wrong answer ;(. Correct answer was '${primeOrNot}'.\nLet\'s try again, ${name}!`);
+}
