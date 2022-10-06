53a48563bdb2d45010fc108084c67fa062ffcac4
Alexandra Adelson <ellysmith@Alexandras-MacBook-Pro.local>
2022-10-06 10:38:05 +0200

    bin fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,63 +1,7 @@
-import readlineSync from 'readline-sync';
+import { firstGame } from '../src/games/brain-calc-mechanism.js'
 
-import * as dialog from './../cli.js';
 
-import { name } from './../cli.js';
+import { secondGame } from '../src/games/brain-even-mechanism.js';
 
-const secondGame = () => {
-  const secondGameArr = [];
+secondGame();
 
-  const secondGameIntroduction = 'Answer "yes" if the number is even, otherwise answer "no".';
-  secondGameArr.push(secondGameIntroduction);
-
-  const randomNumber = Math.floor(Math.random() * 20);
-  const secondGameQuestion = `Question: ${randomNumber}`;
-  secondGameArr.push(secondGameQuestion);
-
-  let isNumberEven;
-
-  if (randomNumber % 2 === 0) {
-    isNumberEven = 'yes';
-  }
-  else {
-    (isNumberEven = 'no');
-  }
-
-  secondGameArr.push(isNumberEven);
-  return secondGameArr;
-};
-
-let game = secondGame(1);
-console.log(game[0]);
-console.log(game[1]);
-
-const answer = readlineSync.question('Your answer: ');
-if (answer === game[2].toString()) {
-  console.log('Correct!');
-  let game = secondGame(2);
-  console.log(game[1]);  
-  const answer = readlineSync.question('Your answer: ');
-    if (answer === game[2].toString()) {
-      let game = secondGame(2);
-      console.log(game[1]);  
-      const answer = readlineSync.question('Your answer: ');
-      if (answer === game[2].toString()) {
-      console.log(`Correct!\nCongratulations, ${name}!`);
-      }
-      else {
-        console.log(`Wrong!\n"${answer}" is wrong answer ;(. Correct answer was '${game[2]}'\nLet's try again, ${name}!`);
-        
-      }
-    }
-    else {
-      console.log(`Wrong!\n"${answer}" is wrong answer ;(. Correct answer was '${game[2]}'\nLet's try again, ${name}!`);
-      
-    }
-  }
-  else {
-    console.log(`Wrong!\n"${answer}" is wrong answer ;(. Correct answer was '${game[2]}'\nLet's try again, ${name}!`);
-    }
-
-    
-
-export { secondGame };
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,15 +3,4 @@ import readlineSync from 'readline-sync';
 
 import * as dialog from '../src/cli.js';
 
-import * as evennumber from './brain-even.js';
-
-import * as calc from './brain-calc.js';
-
-import * as progression from './brain-progression.js';
-
-import * as gcd from './brain-gdc.js';
-
-import * as primenumber from './brain-prime.js';
-
-
 
