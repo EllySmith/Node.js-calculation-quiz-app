0c7dfc80f999cae3581cc23c0daba1011cb27d5f
Alexandra Adelson <ellysmith@Alexandras-MacBook-Pro.local>
2022-10-10 17:43:58 +0200

    switch

--- src/games/brain-calc-mechanism.js
+++ src/games/brain-calc-mechanism.js
@@ -6,17 +6,18 @@ const getQuestionAndAnswer = () => {
 
   const randomNumber1 = getRandomNumber(0, 20);
   const randomNumber2 = getRandomNumber(0, 20);
-  const randomOperatorIndex = getRandomNumber(0, 3);
+  const randomOperatorIndex = getRandomNumber(1, 4);
 
   let randomOperator;
-  if (randomOperatorIndex >= 0 && randomOperatorIndex < 1) {
-    randomOperator = '+';
-  }
-  if (randomOperatorIndex >= 1 && randomOperatorIndex < 2) {
-    randomOperator = '-';
-  }
-  if (randomOperatorIndex >= 2 && randomOperatorIndex <= 3) {
-    randomOperator = '*';
+  switch (randomOperatorIndex) {
+    case 1: randomOperator = '+';
+      break;
+    case 2: randomOperator = '-';
+      break;
+    case 3: randomOperator = '*';
+      break;
+    default:
+      randomOperator = '+';
   }
 
   const questionGameOne = `Question: ${randomNumber1} ${randomOperator} ${randomNumber2}`;
